3fc8c0e15cea8e9a4cbdd5d74fb067c7d590b4b5
Slh0031 <2536272032@qq.com>
2022-11-27 20:55:38 +0800

    slh

---
+++ STM32f1c8t6_PS2_标准库历程/HAREVER/beep/beep.h
@@ -0,0 +1,10 @@
+#ifndef _BEEP__H
+#define _BEEP__H
+
+
+
+void beep_Init(void);
+	
+#define beep PBout(8)
+
+#endif
---
+++ STM32f1c8t6_PS2_标准库历程/HAREVER/led/led.h
@@ -0,0 +1,17 @@
+#ifndef _LED__H
+#define _LED__H
+
+void led_Init(void);
+
+#define LED PCout(13)
+
+#endif
+
+
+
+
+
+
+
+
+
